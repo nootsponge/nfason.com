74867306839efcea73806bbb26dfc55ec58a5191
Andrew <andrew@nootsponge.com>
2022-04-13 12:53:05 -0500

    Initial commit

---
+++ src/Biz.js
@@ -0,0 +1,22 @@
+function Biz() {
+    return(
+      <>
+        <span class="beeg">NICHOLAS<b>FASON</b></span><br />
+        <span class="smol">Backend Developer // I suck at UI</span>
+        <a href="/">Biz mode off</a>
+        <h3>CONTACT INFORMATION</h3>
+        <ul>
+          <li>Personal/Business Inquiries: <a href="mailto:nicholasfason@gmail.com">nicholasfason@gmail.com</a></li>
+          <li>Educational Inquiries: <a href="mailto:nfason@uab.edu">nfason@uab.edu</a></li>
+        </ul>
+
+        <h3>Some facts</h3>
+        <ol>
+          <li>I'm a Computer Science student studying at the University of Alabama at Birmingham. Go Blazers!</li>
+          <li>I'm from the south.</li>
+        </ol>
+      </>
+    )
+}
+
+export default Biz;
---
+++ src/Personal.js
@@ -0,0 +1,33 @@
+function Personal() {
+    return (
+      <>
+        <span class="beeg">NICHOLAS<b>FASON</b></span><br />
+        <span class="smol">Backend Developer // I suck at UI</span>
+        <a href="/biz">Biz mode on</a>
+        <h3>You got something to say to me?</h3>
+        <ul>
+          <li>Personal/Business Inquiries: <a href="mailto:nicholasfason@gmail.com">nicholasfason@gmail.com</a></li>
+          <li>Educational Inquiries: <a href="mailto:nfason@uab.edu">nfason@uab.edu</a></li>
+          <li>If you need me more urgently than an email then you should already have my phone number.</li>
+        </ul>
+
+        <h3>Some facts</h3>
+        <ol>
+          <li>I'm a Computer Science student studying at the University of Alabama at Birmingham. Go Blazers!</li>
+          <li>I'm from the south.</li>
+          <li>I pronounce "pecan" like puh-kahn, not pee-can.</li>
+          <li>I'm a Christian.</li>
+          <li>I'm literally obsessed with music.</li>
+        </ol>
+
+        <h3>Hobbies include...</h3>
+        <ul>
+          <li>Producing short films</li>
+          <li>Video games</li>
+          <li>Live event production (A/V, livestreaming, etc.)</li>
+        </ul>
+      </>
+    )
+}
+
+export default Personal;
