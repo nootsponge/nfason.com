c8e0b54a8a2b50ce0f80c79e7b7a4524ffd5a1c0
Andrew <andrew@nootsponge.com>
2022-06-18 13:48:23 -0500

    Add resume

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,7 @@
 function Header() {
     return (
         <>
+            <a class="far-right" href="https://cdn.n00t.io/file/ntspng/Resume.pdf" target="_blank" rel="noreferrer">Download R&eacute;sum&eacute;</a>
             <span class="beeg">NICHOLAS<b>FASON</b></span><br />
             <span class="smol">Backend Developer // I suck at UI</span>
         </>
