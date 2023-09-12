2d898193f65c2f2733ce6872858644fb72e834ef
Nicholas Fason <me@nfason.com>
2023-09-12 14:31:40 -0500

    Add description

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Nicholas Fason';
-export const SITE_DESCRIPTION = '';
+export const SITE_DESCRIPTION = 'Computer Science + Communication Studies Undergrad and Researcher';
